461b41acbc402d5adf1caeebd46c10c1450540c8
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2017-04-25 11:03:08 +0200

    wrap imports of main modules for backward compatibility

--- source/ftrack_connect_3dsmax/__init__.py
+++ source/ftrack_connect_3dsmax/__init__.py
@@ -1,4 +1,7 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2016 ftrack
 
+import ftrack_connect_3dsmax.legacy as ftrack_connect_3dsmax
+import ftrack_connect_3dsmax.publish as ftrack_connect_3dsmax_publish
+
 from ftrack_connect_3dsmax.legacy._version import __version__
